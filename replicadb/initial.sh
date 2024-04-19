fce8798f33186e6298ffbb0dc4075e5cb94a774e
Amirali Salari <amirali.salar@outlook.com>
2024-04-20 00:52:09 +0330

    Added initialize script and MariaDB configuration for replica database.

---
+++ replicadb/initial.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Install MySQL client.
+apt update
+apt install -y mysql-client
+
+# Execute SQL commands.
+mysql -u root -p'secret_root_password' -e"CHANGE MASTER TO
+  MASTER_HOST='amirali_salari_cangrow-mariadb_master-1',
+  MASTER_USER='replication_user',
+  MASTER_PASSWORD='bigs3cret',
+  MASTER_PORT=3306,
+  MASTER_LOG_FILE='$(cat /opt/masterdb/file_name)',
+  MASTER_LOG_POS=$(cat /opt/masterdb/file_position),
+  MASTER_CONNECT_RETRY=10;"
